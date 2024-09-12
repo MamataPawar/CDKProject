833cca66bf07450430a58805f0ff963259a8b4a2
MamataPawar <mammu89@live.com>
2024-09-12 19:26:19 +0530

    Initial commit

---
+++ src/CdkApp/CdkAppStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkApp
+{
+    public class CdkAppStack : Stack
+    {
+        internal CdkAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
